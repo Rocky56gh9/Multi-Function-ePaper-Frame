450734090c81efc82a0e5388554162de91ab4534
Rocky56gh9 <154940519+Rocky56gh9@users.noreply.github.com>
2024-05-24 15:03:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,14 @@ git clone https://github.com/waveshare/e-Paper.git
 # Configure git
 git config --global http.postBuffer 524288000
 
+# Copy image files to appropriate location
+mkdir -p /home/pi/multimode-epaper-frame/images
+cp *.bmp /home/pi/multimode-epaper-frame/images/
+
+# Copy Python scripts to appropriate location
+mkdir -p /home/pi/multimode-epaper-frame/scripts
+cp *.py /home/pi/multimode-epaper-frame/scripts/
+
 # Enable SPI interface (This will open the raspi-config tool, requiring user interaction)
 sudo raspi-config nonint do_spi 0
 
