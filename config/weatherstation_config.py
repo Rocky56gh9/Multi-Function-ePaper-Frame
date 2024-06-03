129774ceb93989730d47845534f215fed2ebc63e
Rocky56gh9 <154940519+Rocky56gh9@users.noreply.github.com>
2024-06-03 14:28:54 -0400

    Update weatherstation_config.py

--- config/weatherstation_config.py
+++ config/weatherstation_config.py
@@ -1,8 +1,12 @@
 import os
+import requests
+
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Path to the template file
-TEMPLATE_PATH = 'templates/weatherstation_template.py'
-SCRIPTS_PATH = 'scripts/weatherstation.py'
+TEMPLATE_PATH = os.path.join(script_dir, '../templates/weatherstation_template.py')
+SCRIPTS_PATH = os.path.join(script_dir, '../scripts/weatherstation.py')
 
 def configure_weatherstation():
     print("Configuring Weather Station Script")
@@ -11,12 +15,24 @@ def configure_weatherstation():
     
     home_dir = os.getenv("HOME")
 
+    # Fetch location data
+    location_url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_code},us&appid={api_key}"
+    response = requests.get(location_url)
+    if response.status_code == 200:
+        data = response.json()
+        lat = data['coord']['lat']
+        lon = data['coord']['lon']
+        temp = data['main']['temp']
+        weather_description = data['weather'][0]['description']
+    else:
+        raise Exception("Error fetching location data")
+
     # Read the template file
     with open(TEMPLATE_PATH, 'r') as file:
         template_content = file.read()
 
     # Replace placeholders with user inputs
-    script_content = template_content.format(api_key=api_key, zip_code=zip_code, home_dir=home_dir)
+    script_content = template_content.format(api_key=api_key, zip_code=zip_code, home_dir=home_dir, lat=lat, lon=lon, temp=temp, weather_description=weather_description)
 
     # Write the configured script to the scripts directory
     with open(SCRIPTS_PATH, 'w') as file:
