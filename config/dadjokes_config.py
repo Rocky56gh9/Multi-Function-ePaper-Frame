b075b5d2b8900cb65454d0768a23ef0374f71303
Rocky56gh9 <154940519+Rocky56gh9@users.noreply.github.com>
2024-06-03 10:14:13 -0400

    Update dadjokes_config.py

--- config/dadjokes_config.py
+++ config/dadjokes_config.py
@@ -9,13 +9,15 @@ def configure_dadjokes():
     client_id = input("Enter your Reddit API client ID: ")
     client_secret = input("Enter your Reddit API client secret: ")
     user_agent = input("Enter your Reddit API user agent: ")
+    
+    home_dir = os.getenv("HOME")
 
     # Read the template file
     with open(TEMPLATE_PATH, 'r') as file:
         template_content = file.read()
 
     # Replace placeholders with user inputs
-    script_content = template_content.format(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+    script_content = template_content.format(client_id=client_id, client_secret=client_secret, user_agent=user_agent, home_dir=home_dir)
 
     # Write the configured script to the scripts directory
     with open(SCRIPTS_PATH, 'w') as file:
