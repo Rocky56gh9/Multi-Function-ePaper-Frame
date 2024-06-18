878710ac6c59417d04d4eaa830b460aa6a65d25a
Rocky56gh9 <154940519+Rocky56gh9@users.noreply.github.com>
2024-06-18 17:50:41 -0400

    Update run_all_configs.py

--- run_all_configs.py
+++ run_all_configs.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
 
-# List of configuration scripts to run
+# Get the directory of the current script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# List of configuration scripts
 scripts = [
-    'config/dadjokes_showerthoughts_config.py',
-    'config/weatherstation_config.py',
-    'config/crontab_config.py'
+    os.path.join(script_dir, "config/dadjokes_showerthoughts_config.py"),
+    os.path.join(script_dir, "config/weatherstation_config.py"),
+    os.path.join(script_dir, "config/crontab_config.py")
 ]
 
-def run_script(script_name):
+def run_script(script_path):
     try:
-        subprocess.run(['python3', script_name], check=True)
+        subprocess.check_call(["python3", script_path])
+        print(f"Completed {script_path}")
     except subprocess.CalledProcessError as e:
-        print(f"Error occurred while running {script_name}: {e}")
+        print(f"Error occurred while running {script_path}: {e}")
         exit(1)
 
-def main():
+if __name__ == "__main__":
     for script in scripts:
         print(f"Running {script}...")
         run_script(script)
-        print(f"Completed {script}")
-
-if __name__ == "__main__":
-    main()
